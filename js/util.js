38adadbee4d1256c6bfbe28c36030986d506be57
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-09-12 03:42:53 +0400

    Started work on dynamic javascript work

--- js/util.js
+++ js/util.js
@@ -1,11 +1,16 @@
 $(function() {
-	
-	var xs =;
-	var sm =;
-	var md =;
+	checkScreen();
+});
+
+var checkScreen = function(){
+	var md =992;
 
 	var screenSize = $(window).width();
 
-
-
-});
+	//if smaller screen
+	if(screenSize < md){
+		//TODO: remove row-eq-height	
+	}else{
+		//add row-eq-height
+	}
+}
