0fad95e7cbc908c005af80da397bd44a0c8fc086
themakshter <mohammad_ali93@live.com>
2016-02-24 23:18:58 +0000

    Add more code to webpage-generator.py

--- v3-wip/webpage-generator.py
+++ v3-wip/webpage-generator.py
@@ -1,37 +1,40 @@
 html = "<html>"
+folder_to_search_in = "data"
 
 
 def generate_website():
+    add_headers("headers.json")
+    add_about_me("about-me.json")
+    add_education("education.json")
+    add_experience("experience.json")
+    add_skills("skills.json")
+    add_projects("projects.json")
+    add_timeline("timeline.json")
+
+
+def add_headers(file):
     print("TODO")
 
 
-def add_headers():
+def add_about_me(file):
     print("TODO")
 
 
-def add_about_me():
+def add_education(file):
     print("TODO")
 
 
-def add_education():
+def add_experience(file):
     print("TODO")
 
 
-def add_experience():
+def add_skills(file):
     print("TODO")
 
 
-def add_skills():
+def add_projects(file):
     print("TODO")
 
 
-def add_projects():
-    print("TODO")
-
-
-def add_timeline():
-    print("TODO")
-
-
-def add_footer_resources():
+def add_timeline(file):
     print("TODO")
