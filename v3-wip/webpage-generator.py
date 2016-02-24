75c7193c54fedaac6c54af50aed373a2de78c35e
themakshter <mohammad_ali93@live.com>
2016-02-24 23:03:35 +0000

    Use python instead of javascript to generate website

---
+++ v3-wip/webpage-generator.py
@@ -0,0 +1,37 @@
+html = "<html>"
+
+
+def generate_website():
+    print("TODO")
+
+
+def add_headers():
+    print("TODO")
+
+
+def add_about_me():
+    print("TODO")
+
+
+def add_education():
+    print("TODO")
+
+
+def add_experience():
+    print("TODO")
+
+
+def add_skills():
+    print("TODO")
+
+
+def add_projects():
+    print("TODO")
+
+
+def add_timeline():
+    print("TODO")
+
+
+def add_footer_resources():
+    print("TODO")
