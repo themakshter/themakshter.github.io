5e2d72fa7b31cf028c2ac934dc4fdf56d0c43377
themakshter <mohammad_ali93@live.com>
2016-03-01 23:46:46 +0000

    Added methods to add the content of the 'head' tag

--- v3-wip/webpage-generator.py
+++ v3-wip/webpage-generator.py
@@ -1,19 +1,54 @@
+import json
+
 html = "<html>"
-folder_to_search_in = "data"
 
 
 def generate_website():
-    add_headers("headers.json")
-    add_about_me("about-me.json")
-    add_education("education.json")
-    add_experience("experience.json")
-    add_skills("skills.json")
-    add_projects("projects.json")
-    add_timeline("timeline.json")
+    global html
+    add_headers("data/headers.json")
+    html += "\n<body>"
+    add_body()
 
 
-def add_headers(file):
-    print("TODO")
+def add_body():
+    add_about_me("data/about-me.json")
+    add_education("data/education.json")
+    add_experience("data/experience.json")
+    add_skills("data/skills.json")
+    add_projects("data/projects.json")
+    add_timeline("data/timeline.json")
+
+
+def add_headers(filename):
+    global html
+    data = read_json_file(filename)
+    html += "\n\t<head>\n\t\t<title>" + data['title'] + "</title>"
+    for link in data['links']:
+        html += "\n\t\t"
+        if link['type'] == 'css':
+            html += get_css_link(link['source'])
+        elif link['type'] == 'js':
+            html += get_js_link(link['source'])
+        elif link['type'] == 'font':
+            html += get_font_link(link['source'])
+    html += "\n\t</head>"
+    print(html)
+
+
+def read_json_file(filename):
+    return json.loads(open(filename).read())
+
+
+def get_css_link(src):
+    return "<link href=\"" + src + "\" rel=\"stylesheet\" type=\"text/css\" />"
+
+
+def get_js_link(src):
+    return "<script src=\"" + src + "\"></script>"
+
+
+def get_font_link(src):
+    return "<link href=\"" + src + "\" rel=\"stylesheet\" />"
 
 
 def add_about_me(file):
@@ -38,3 +73,6 @@ def add_projects(file):
 
 def add_timeline(file):
     print("TODO")
+
+
+generate_website()
